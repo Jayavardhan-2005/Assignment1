9089c063eacbbd9e670d6bb15454f20882eab163
Jayavardhana A <jayavardhana29@gmail.com>
2025-04-27 20:10:34 +0530

    Add files via upload

---
+++ Assignment1_Task1.py
@@ -0,0 +1,6 @@
+fnum=int(input("Enter first number : "))
+snum=int(input("Enter second number : "))
+print("Addition: " , fnum+snum)
+print("Subtraction: " , fnum-snum)
+print("Multiplication: " , fnum*snum)
+print("Division: " , fnum/snum)
---
+++ Assignment1_Task2.py
@@ -0,0 +1,3 @@
+fname=input("Enter your first name: ")
+lname=input("Enter your second name: ")
+print("Hello, ",fname,lname+"!"," Welcome to the python program")
